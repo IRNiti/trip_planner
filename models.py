1585941525cbc5d19eb703d72791e89020d55b56
IRNiti <ioana.niti@mail.mcgill.ca>
2020-01-12 10:27:52 -0800

    feat: completed models for trips, flights and accommodations

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, Integer, Date, DateTime, Boolean, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
@@ -22,3 +22,100 @@ def setup_db(app, database_path=database_path):
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
+
+
+class Trip(db.Model):
+  __tablename__ = 'trips'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String)
+  start_date = Column(Date)
+  end_date = Column(Date)
+  flights = db.relationship('Flight', backref=db.backref('flights', lazy=True))
+  accomodations = db.relationship('Accommodation', backref=db.backref('accommodations', lazy=True))
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'start_date': self.start_date,
+      'end_date': self.end_date
+    }
+
+
+class Flight(db.Model):
+  __tablename__ = 'flights'
+
+  id = Column(Integer, primary_key=True)
+  origin = Column(String)
+  destination = Column(String)
+  time = Column(DateTime)
+  booked = Column(Boolean)
+  trip = Column(Integer, db.ForeignKey('trips.id', ondelete="CASCADE"))
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'origin': self.origin,
+      'destination': self.destination,
+      'time': self.time,
+      'booked': self.booked,
+      'trip': self.trip
+    }
+
+
+class Accommodation(db.Model):
+  __tablename__ = 'accommodations'
+
+  id = Column(Integer, primary_key=True)
+  location = Column(String)
+  start_date = Column(Date)
+  end_date = Column(Date)
+  booked = Column(Boolean)
+  trip = Column(Integer, db.ForeignKey('trips.id', ondelete="CASCADE"))
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'location': self.location,
+      'start_date': self.start_date,
+      'end_date': self.end_date,
+      'booked': self.booked,
+      'trip': self.trip
+    }
+
+
+
