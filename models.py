cd52cbae4e2d721cb1fb35ed5ff4506e244dd88e
IRNiti <ioana.niti@mail.mcgill.ca>
2020-01-18 13:46:24 -0800

    modifying db url reference

--- models.py
+++ models.py
@@ -2,9 +2,9 @@ from sqlalchemy import Column, String, Integer, Date, DateTime, Boolean, create_
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-#database_path = os.environ['DATABASE_URL']
-database_name = "trip"
-database_path = "postgresql://postgres@{}/{}".format('localhost:5432', database_name)
+database_path = os.environ['DATABASE_URL']
+#database_name = "trip"
+#database_path = "postgresql://postgres@{}/{}".format('localhost:5432', database_name)
 
 
 db = SQLAlchemy()
